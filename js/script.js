33787e557e92c1ffa6f219d121a315d79c9ae7e2
nikhilwebdeveloper <108711205+Nikhilsain748@users.noreply.github.com>
2022-09-17 19:44:28 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+// preloader
+let preloader = document.getElementById('loading');
+
+function myFunction(){
+    preloader.style.display = "none";
+}
+
 function openMenu(){
     document.getElementById("navbar").style.left = "0px";
+    document.body.style.overflowY = "hidden"
 }
 
 function closeMenu(){
     document.getElementById("navbar").style.left = "-280px";
+    document.body.style.overflowY = "auto"
 }
 
 function closeIntro(){
     document.getElementById("intro-container").style.display = "none"
     document.body.style.overflowY = "auto"
+    document.getElementById("dark-bg").style.display = "none"
 }
+
+
+
+
+
 
 // alert("Hey, Welcome to my page Thank you for visit my page keep loving and supporting 😍")
